af11cadea09820ac9d7c34a37294f7266a0a300f
JasonPC <jasonwong287962@gmail.com>
2023-03-16 00:40:35 -0500

    added about.js text

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,25 @@
 import { Link } from 'react-router-dom';
+import '../css/about.css'
 import Nav from './Nav';
 const About = () => {
   return (
-    <main>
-        <h1>About Jason</h1>
-    
+    <main className='about'>
+        <h1>About Jason </h1>
+     <h4>Jan 2022 - Before The Odin Project</h4>
+     <p>
+      I started working at Juicy Seafood, a restaurant, where I was a manager. After working 70-80 Hours a week until September of 2022, 
+      I started working around 60 hours a week... Thank God!
+      With a little more time to spare, I started playing Valorant and eventually reached Immortal 1. At this point, having reached the top 0.1%, and 
+      feeling like I've been wasting my time with no improvement in my personal life.
+      I decided to put my efforts elsewhere. This is when I decided to gradually put more effort into TOP
+      
+    </p>
+    <h4>Nov 2022 - The Odin Project</h4>
+      <p>
+        I truly started engaging with The Odin Project around Nov 2022. Having done multiple projects, I have 
+        realized I truly enjoy problem solving and coding. You can view my projects on this website, or on Github It was fun completing these projects despite working 60 hour weeks.
+        And I will continue to learn with my spare time
+      </p>
 
 
     </main>
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,9 +4,38 @@ const Main = () => {
   return (
     <main >
         <h1>Jason's Projects</h1>
-    
+    <h3>
+      Twitter Clone (TOOT):
+       <a href='https://twitter-clone-33714.web.app/'>Website </a> 
+      <a href='https://github.com/jw287962/twitter-clone'>Github</a>
+    </h3>
+    <h3>
+      Knight Travils Clone:
+       <a href='https://jw287962.github.io/knight_travails/'>Website </a> 
+      <a href='https://github.com/jw287962/knight_travails'>Github</a>
+    </h3>
+    <h3>
+      Weather App:
+       <a href='https://jw287962.github.io/weather/'>Website </a> 
+      <a href='https://github.com/jw287962/weather'>Github</a>
+    </h3>
 
-
+    <h3>
+     Todo App:
+       <a href='https://jw287962.github.io/todo/'>Website </a> 
+      <a href='https://github.com/jw287962/todo'>Github</a>
+    </h3>
+    <h3>
+     Battleship Game:
+       <a href='https://jw287962.github.io/battleship/'>Website </a> 
+      <a href='https://github.com/jw287962/battleship'>Github</a>
+    </h3>
+    <h3>
+      Calculator:
+       <a href='https://jw287962.github.io/calculator/'>Website </a> 
+      <a href='https://github.com/jw287962/calculator'>Github</a>
+    </h3>
+ 
     </main>
   )
 }
