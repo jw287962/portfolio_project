b4f309a261fc31c83f420a1bb7e14fb2f6fce9fd
JasonPC <jasonwong287962@gmail.com>
2023-03-16 00:13:37 -0500

    added main home page

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import Nav from './Nav';
+const Main = () => {
+  return (
+    <main >
+        <h1>Jason's Projects</h1>
+    
+
+
+    </main>
+  )
+}
+
+export default Main;
