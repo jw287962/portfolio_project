b6ca3faff691848b9196cb497b89ff99ee9ca426
JasonPC <jasonwong287962@gmail.com>
2023-03-16 00:04:23 -0500

    for about page

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import Nav from './Nav';
+const About = () => {
+  return (
+    <main >
+      <Nav></Nav>
+        <h1>About Jason</h1>
+    
+
+
+    </main>
+  )
+}
+
+export default About;
