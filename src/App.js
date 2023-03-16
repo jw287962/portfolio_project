87997c7db231602e7cb6db2923bb3c9e0bf7c45d
JasonPC <jasonwong287962@gmail.com>
2023-03-16 00:13:47 -0500

    added nav bar css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Nav from './components/Nav';
+import Main from './components/Main';
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
+      <Main></Main>
       
     </div>
   );
