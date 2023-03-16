8820c12fbe7a558e56828822334fe1c1afb88fa9
JasonPC <jasonwong287962@gmail.com>
2023-03-16 00:04:02 -0500

    made a nav bar with home and about

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,19 @@
 import '../css/nav.css'
 import { Link } from 'react-router-dom';
+
 const Nav = () => {
   return (
     <nav className="mainNav">
         <h1>Jason Wong</h1>
-        <Link >
-          <about></about>
-        </Link>
 
+        <ul className='navList'>
+          <li>
+            <Link to={{ pathname: "/"}}>Home  </Link>
+          </li>
+          <li>
+            <Link to={{ pathname: "/about"}}>About  </Link>
+          </li>
+        </ul>
 
     </nav>
   )
