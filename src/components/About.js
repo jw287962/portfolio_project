d5554ffb52e755646bca3fa0956aac228506a3ac
JasonPC <jasonwong287962@gmail.com>
2023-03-17 00:41:53 -0500

    updated some text

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,16 @@ const About = () => {
   return (
     <main className='about'>
         <h1>About Jason </h1>
+        <p>Although I graduated as a Management Information Systems graduate at University of Georgia in 2020, I ended up not pursuing a job in my field.
+          I am currently 25 years old, and have discovered a new passion, programming. You can view some of my work here
+          while learning through the Odin Project. I am always eager for a challenge, finding enjoyment in problem solving and learning 
+          new things. Some of my other hobbies are sports and gaming.
+           </p>
      <h4>Jan 2022 - Before The Odin Project</h4>
      <p>
       I started working at Juicy Seafood, a restaurant, where I was a manager. After working 70-80 Hours a week until September of 2022, 
       I started working around 60 hours a week... Thank God!
-      With a little more time to spare, I started playing Valorant and eventually reached Immortal 1. At this point, having reached the top 0.1%, and 
+      With a little more time to spare, I started playing Valorant and eventually reached Immortal 1. At this point, having reached the top 1%, and 
       feeling like I've been wasting my time with no improvement in my personal life.
       I decided to put my efforts elsewhere. This is when I decided to gradually put more effort into TOP
       
@@ -21,6 +26,8 @@ const About = () => {
         And I will continue to learn with my spare time
       </p>
 
+
+    <h2>Skills </h2>
 
     </main>
   )
