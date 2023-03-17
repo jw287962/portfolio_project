36c2e08704a8f0c0a482aa60e9dd7bc180ac7b2c
JasonPC <jasonwong287962@gmail.com>
2023-03-17 01:24:17 -0500

    header title to jason portfolio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 import Nav from './components/Nav';
+import { useEffect } from 'react';
 import Main from './components/Main';
 function App() {
+  useEffect( () => {
+    document.title = 'Jason Portfolio';
+  }, []);
+
   return (
     <div className="App">
       <Main></Main>
