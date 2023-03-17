4be536ee87ad60f74d8f288418696ca970246e98
JasonPC <jasonwong287962@gmail.com>
2023-03-17 00:41:48 -0500

    added image and updated button for projects

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,54 +1,89 @@
 import { Link } from 'react-router-dom';
+import battleship from '../img/battleship.png'
+import twitter from '../img/twitterclone.png'
+import weather from '../img/weather.png'
+import todo from '../img/todo.png'
+import calculator from '../img/calculator.png'
+import github from '../img/Github.png';
 import Nav from './Nav';
+import arrow from '../img/arrow.png'
 const Main = () => {
   return (
     <main  >
         <h1>Jason's Projects</h1>
     <div className='home'>
       <h3>
-        Twitter Clone (TOOT):
-         <div className='links'>
-           <button className='link'><a href='https://twitter-clone-33714.web.app/'>Website </a> </button>
-                 <button className='link'><a href='https://github.com/jw287962/twitter-clone'>Github</a></button>
-         </div>
+          <div className='links'>
+          <img src={twitter} className="projects"></img>
+          <h3>TOOT (Twitter Clone):</h3>
+
+            <div>
+      <a href='https://twitter-clone-33714.web.app/'><img className="weblink" height="30" width="30" src={arrow}></img></a>
+                    <a href='https://github.com/jw287962/twitter-clone'><img height="38" width="38" src={github} ></img></a>
+            </div>
+          </div>
+      </h3>
+   
+      <h3>
+            <div className='links'>
+            <img src={weather} className="projects"></img>
+            <h3>Weather App:</h3>
+
+            <div>
+             <a href='https://jw287962.github.io/weather/'><img className="weblink" height="30" width="30" src={arrow}></img></a>
+             <a href='https://github.com/jw287962/weather'><img height="40" width="40" src={github} ></img></a>
+            </div>
+            </div>
       </h3>
       <h3>
-        Knight Travils Clone:
-        <div className='links'>
-         <button className='link'><a href='https://jw287962.github.io/knight_travails/'>Website </a> </button>
-        <button className='link'><a href='https://github.com/jw287962/knight_travails'>Github</a></button>
-        </div>
-      </h3>
-      <h3>
-        Weather App:
-        <div className='links'>
-        <button className='link'><a href='https://jw287962.github.io/weather/'>Website </a> </button>
-        <button className='link'><a href='https://github.com/jw287962/weather'>Github</a></button>
-        </div>
-      </h3>
-      <h3>
-       Todo App:
-       <div className='links'>
-       <button className='link'><a href='https://jw287962.github.io/todo/'>Website </a> </button>
-        <button className='link'><a href='https://github.com/jw287962/todo'>Github</a></button>
-       </div>
+          <div className='links'>
+          <img src={todo} className="projects"></img>
+            <h3>Todo App:</h3>
+          <div>
+        
+              <a href='https://jw287962.github.io/todo/'><img className="weblink" height="30" width="30" src={arrow}></img></a>
+             <a href='https://github.com/jw287962/todo'><img height="40" width="40" src={github} ></img></a>
+          </div>
+          </div>
       
       </h3>
       <h3>
-       Battleship Game:
-       <div className='links'>
-       <button className='link'><a href='https://jw287962.github.io/battleship/'>Website </a> </button>
-        <button className='link'><a href='https://github.com/jw287962/battleship'>Github</a></button>
-        </div>
-      
-      </h3>
+          <div className='links'>
+          <img src={battleship} className="projects"></img>
+          <h3>Battleship Game:</h3>
+
+          <div>
+       
+              <a href='https://jw287962.github.io/battleship/'><img className="weblink" height="30" width="30" src={arrow}></img></a>
+             <a href='https://github.com/jw287962/battleship'><img height="40" width="40" src={github} ></img></a>
+          </div>
+            </div>
+          
+          </h3>
       <h3>
-        Calculator:
+        
+          <div className='links'>
+          <img src={calculator} className="projects"></img>
+            <h3> Calculator:</h3>
+          <div>
+        
+            <a href='https://jw287962.github.io/calculator/'><img className="weblink" height="30" width="30" src={arrow}></img></a>
+             <a href='https://github.com/jw287962/calculator'><img height="40" width="40" src={github} ></img></a>
+          </div>
+          </div>
+        
+          </h3>
+      <h3>
+     
+        
         <div className='links'>
-        <button className='link'><a href='https://jw287962.github.io/calculator/'>Website </a> </button>
-        <button className='link'><a href='https://github.com/jw287962/calculator'>Github</a></button>
+
+        <h3>Knight Travils:</h3>
+          <div>
+                    <a href='https://jw287962.github.io/knight_travails/'><img className="weblink" height="30" width="30" src={arrow}></img></a>
+             <a href='https://github.com/jw287962/knight_travails'><img height="40" width="40" src={github} ></img></a>
+          </div>
         </div>
-      
       </h3>
     </div>
     </main>
