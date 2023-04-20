0260c82788ecb020881ac606ecf4d2a82a133c08
JasonPC <jasonwong287962@gmail.com>
2023-04-20 00:55:51 -0500

    helper tool for importing images of my skills

---
+++ src/components/skillImage.js
@@ -0,0 +1,26 @@
+import reactlogo from "../img/skills/react.png";
+
+import css from "../img/skills/css.png";
+import express from "../img/skills/express.png";
+import git from "../img/skills/git.png";
+import html from "../img/skills/html.webp";
+import javascript from "../img/skills/javascript.png";
+import mongodb from "../img/skills/mongodb.png";
+import mysql from "../img/skills/mysql.png";
+import nodejs from "../img/skills/nodejs.png";
+import npm from "../img/skills/npm.png";
+import webpack from "../img/skills/webpack.png";
+
+export const skillImages = {
+  reactlogo,
+  css,
+  express,
+  git,
+  html,
+  javascript,
+  mongodb,
+  mysql,
+  nodejs,
+  npm,
+  webpack,
+};
