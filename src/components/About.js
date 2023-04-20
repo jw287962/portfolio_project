22a706b14eb57c4f76df1829017a7fbac8005533
JasonPC <jasonwong287962@gmail.com>
2023-04-20 01:04:26 -0500

    skills component updated in about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,79 +1,83 @@
-import { Link } from 'react-router-dom';
-import '../css/about.css'
-import Nav from './Nav';
-import { useEffect } from 'react';
-import jason from '../img/jason.png'
+import { Link } from "react-router-dom";
+import "../css/about.css";
+import Nav from "./Nav";
+import { useEffect } from "react";
+
+import jason from "../img/jason.png";
+import Skills from "../components/Skills";
+
 const About = () => {
-
-  useEffect(()=> {
-
-    let options = {
-     
-    }
-    const observer = new IntersectionObserver((entries,options) => {
+  useEffect(() => {
+    let options = {};
+    const observer = new IntersectionObserver((entries, options) => {
       entries.forEach((entry) => {
         console.log(entry);
-        if(entry.isIntersecting){
-          entry.target.classList.add('show')
-
-        }else{
-          entry.target.classList.remove('show')
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
         }
-      })
-    })
-    const hiddenElements = document.querySelectorAll('.hidden');
+      });
+    });
+    const hiddenElements = document.querySelectorAll(".hidden");
     hiddenElements.forEach((el) => observer.observe(el));
-
-  })
+  });
 
   return (
-    <main className='about'>
-      <img src={jason} height='250px'></img>
-        <div className='hidden'>
-          <h1>About Jason </h1>
-          <p>Although I graduated as a Management Information Systems graduate at University of Georgia in 2020, I ended up not pursuing a job in my field.
-            I am currently 25 years old, and have discovered a new passion, programming, through The Odin Project. 
-            'You can view my projects <a href="/portfolio_project/Home">here</a>.
-            I am always eager for a challenge, finding enjoyment in problem solving and learning
-            new things. Outside of programming, I enjoy hobbies such as playing sports and games.
-             </p>
-        </div>
-     <div className="hidden">
-       <h4>Jan 2022 - Before The Odin Project</h4>
-       <p>
-        I started working at Juicy Seafood, a restaurant, where I was a manager. After working 70-80 Hours a week until September of 2022,
-        I was able to lower that to around 60 hours a week... Thank God!
-        With a little more time to spare, I started playing Valorant and eventually reached Immortal 1 in just a few months. At this point, having reached the top 1%, and
-        feeling like I've been wasting my time with no improvement in my personal life, I began looking elsewhere.
-       However, traits such as competitiveness,
-        self-drive, and an innate desire for self-improvement have always been following me through these periods.  With that,
-        I decided to put my efforts in something new. This is when I decided to really dive into TOP.
-        
-           </p>
-     </div>
-    <div className="hidden">
-      <h4>Nov 2022 - The Odin Project</h4>
+    <main className="about">
+      <img src={jason} height="250px"></img>
+      <div className="hidden">
+        <h1>About Jason </h1>
         <p>
-          I truly started engaging with The Odin Project around Nov 2022. As I solved problem after problem, I soon realized how
-          much fun it is. I have enjoyed making all the different projects, and I am looking to do more in a proffessional setting.
-           You can view my projects on this website (with more on Github).
-            It was fun completing these projects despite working 60 hour weeks.
-          and I look foward to becoming a full stack developer.
+          Although I graduated as a Management Information Systems graduate at
+          University of Georgia in 2020, I ended up not pursuing a job in my
+          field. I am currently 25 years old, and have discovered a new passion,
+          programming, through The Odin Project. 'You can view my projects{" "}
+          <a href="/portfolio_project/Home">here</a>. I am always eager for a
+          challenge, finding enjoyment in problem solving and learning new
+          things. Outside of programming, I enjoy hobbies such as playing sports
+          and games.
         </p>
-    </div>
+      </div>
+      <div className="hidden">
+        <h4>Jan 2022 - Before The Odin Project</h4>
+        <p>
+          I started working at Juicy Seafood, a restaurant, where I was a
+          manager. After working 70-80 Hours a week until September of 2022, I
+          was able to lower that to around 60 hours a week... Thank God! With a
+          little more time to spare, I started playing Valorant and eventually
+          reached Immortal 1 in just a few months. At this point, having reached
+          the top 1%, and feeling like I've been wasting my time with no
+          improvement in my personal life, I began looking elsewhere. However,
+          traits such as competitiveness, self-drive, and an innate desire for
+          self-improvement have always been following me through these periods.
+          With that, I decided to put my efforts in something new. This is when
+          I decided to really dive into TOP.
+        </p>
+      </div>
+      <div className="hidden">
+        <h4>Nov 2022 - The Odin Project</h4>
+        <p>
+          I truly started engaging with The Odin Project around Nov 2022. As I
+          solved problem after problem, I soon realized how much fun it is. I
+          have enjoyed making all the different projects, and I am looking to do
+          more in a proffessional setting. You can view my projects on this
+          website (with more on Github). It was fun completing these projects
+          despite working 60 hour weeks, and I would love to apply my skills
+          further as a software developer.
+        </p>
+      </div>
 
+      <Skills></Skills>
 
-    <div className="hidden">
-      <h1>Skills </h1>
-       <p>REACT | JAVASCRIPT | HTML | NODEJS | NPM | WEBPACK | GIT | MONGODB | AND LEARNING </p>
-    </div>
-
-     <div className="hidden">
-       <h1>Contact Me:</h1>
-       <h4><strong>Email</strong>: jason.wongdevwork@gmail.com</h4>
-     </div>
+      <div className="hidden">
+        <h1>Contact Me:</h1>
+        <h4>
+          <strong>Email</strong>: jason.wongdevwork@gmail.com
+        </h4>
+      </div>
     </main>
-  )
-}
+  );
+};
 
 export default About;
---
+++ src/components/Skills.js
@@ -0,0 +1,55 @@
+import { skillImages } from "./skillImage";
+
+const Skills = () => {
+  const imgSize = 50;
+  return (
+    <div>
+      <h1>Skills </h1>
+      <div class="skills">
+        <p className="skill">
+          REACT <img src={skillImages.reactlogo} height={imgSize}></img>
+        </p>
+        <p className="skill">
+          JAVASCRIPT <img src={skillImages.javascript} height={imgSize}></img>
+        </p>
+        <p className="skill">
+          HTML <img src={skillImages.html} height={imgSize}></img>
+        </p>
+        <p className="skill">
+          CSS <img src={skillImages.css} height={imgSize}></img>
+        </p>
+      </div>
+
+      <div class="skills">
+        <p className="skill">
+          NODEJS <img src={skillImages.nodejs} height={imgSize}></img>
+        </p>
+        <p className="skill">
+          NPM <img src={skillImages.npm} height={imgSize}></img>
+        </p>
+        <p className="skill">
+          WEBPACK <img src={skillImages.webpack} height={imgSize}></img>
+        </p>
+        <p className="skill">
+          GIT <img src={skillImages.git} height={imgSize}></img>
+        </p>
+      </div>
+
+      <div class="skills">
+        <p className="skill">
+          EXPRESS <img src={skillImages.express} height={imgSize}></img>
+        </p>
+        <p className="skill">
+          MONGODB <img src={skillImages.mongodb} height={imgSize}></img>
+        </p>
+        <p className="skill">
+          MYSQL <img src={skillImages.mysql} height={imgSize}></img>
+        </p>
+        <p className="skill">
+          RESTful API <img src={skillImages.rest} height={imgSize}></img>
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Skills;
