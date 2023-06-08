10c1e9b97d412bc7ed3ccb3462cfdbe993b7b7fe
Jason <jasonwong287962@gmail.com>
2023-06-08 00:24:45 -0500

    update link  resume

--- src/components/Main.js
+++ src/components/Main.js
@@ -203,7 +203,7 @@ const Main = () => {
           <div className="right">
             <a
               className="imgLink"
-              href="https://drive.google.com/file/d/1gTqXxVsTTTF27J2j43TcvT-Vt1VutliI/view?usp=sharing"
+              href="https://drive.google.com/file/d/1oBFNvUJ9Mahf-eqOoG6llNDY3DcUwPix/view?usp=sharing"
             >
               <Icon path={mdiFilePdfBox} size={2} alt="Jason's Resume" />
             </a>
