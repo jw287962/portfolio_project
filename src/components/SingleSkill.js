9bf3d07bf02038ed86b2a8c03020f96f491f0cb2
Jason <jasonwong287962@gmail.com>
2023-06-27 00:23:25 -0500

    fix one warning

--- src/components/SingleSkill.js
+++ src/components/SingleSkill.js
@@ -2,7 +2,7 @@ const SingleSkill = ({ logo }) => {
   const imgSize = 50;
   return (
     <p className="skill">
-      {logo.name} <img src={logo.picture} height={imgSize}></img>
+      {logo.name} <img src={logo.picture} height={imgSize} alt=""></img>
     </p>
   );
 };
